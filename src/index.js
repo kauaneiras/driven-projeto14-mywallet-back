1a2094b92690d77e6297233e9df69a5f6fc43e32
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-15 21:39:46 -0300

    Initial settings
    import required modules --> OK
    package.json --> OK
    .env --> OK

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import nodemon from 'nodemon';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import Joi from 'joi';
+
+import { MongoClient } from 'mongodb';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const port = 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
