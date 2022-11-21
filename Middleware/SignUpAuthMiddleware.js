ccd5aa4a63a8ee1bc1ddc222113727912ef44ea8
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-21 12:25:55 -0300

    SignUpAuth --> Auth is working
    SignUp Auth with Joi Module.
    name --> OK
    email --> OK
    password --> OK
    checkpassword --> OK

--- Middleware/SignUpAuthMiddleware.js
+++ Middleware/SignUpAuthMiddleware.js
@@ -1,3 +1,20 @@
     import joi from "joi";
 
+  async function SignUpAuthMiddleware(req, res, next) {
+    const {name, email, password, checkpassword} = req.body;
     
+    const SchemaSignUp = joi.object({
+        name: joi.string().min(2).max(50).required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(6).max(20).required(),
+        checkpassword: joi.ref('password'),
+    })
+    const validation = SchemaSignUp.validate({name, email, password, checkpassword});
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    next();
+};
+
+  export default SignUpAuthMiddleware;
--- Routes/SignUpRoutes.js
+++ Routes/SignUpRoutes.js
@@ -1,10 +1,11 @@
 
 
 import { Router } from "express";
+import SignUpAuthMiddleware from "../Middleware/SignUpAuthMiddleware.js";
 import  SignUpController  from "../Controllers/SignUpController.js";
 
 const SignUpRoutes = Router();
 
-SignUpRoutes.post('/sign-up', SignUpController);
+SignUpRoutes.post('/sign-up', SignUpAuthMiddleware, SignUpController);
 
 export default SignUpRoutes;
